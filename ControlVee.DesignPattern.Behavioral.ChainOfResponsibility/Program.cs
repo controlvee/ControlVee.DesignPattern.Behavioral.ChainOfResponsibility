c798dcffe65d8125bba71af3eb458e15b019cd41
controlvee <adam.goerlich@gmail.com>
2021-02-24 21:14:14 -0600

    .

--- ControlVee.DesignPattern.Behavioral.ChainOfResponsibility/Program.cs
+++ ControlVee.DesignPattern.Behavioral.ChainOfResponsibility/Program.cs
@@ -4,21 +4,26 @@
 
     /// <summary>
     /*  
-         Participants:
-        
-            The classes and objects participating in this pattern are:
-                Handler (Approver)
-                    - defines an interface for handling the requests
-                    - (optional) implements the successor link
+        Summary:  Avoid coupling the sender of a request to its receiver 
+        by giving more than one object a chance to handle the request. 
+        Chain the receiving objects and pass the request along the chain 
+        until an object handles it.
 
-                ConcreteHandler (Director|Low, VicePresident|Medium, President|High)
-                    - handles requests it is responsible for
-                    - can access its successor
-                    - if the ConcreteHandler can handle the request, it does so; 
-                    otherwise it forwards the request to its successor
+        Participants -
+        The classes and objects participating in this pattern are:
 
-                Client (ChainApp)
-                    - initiates the request to a ConcreteHandler object on the chain 
+            Handler (Approver)
+                - defines an interface for handling the requests
+                - (optional) implements the successor link
+
+            ConcreteHandler (Director|Low, VicePresident|Medium, President|High)
+                - handles requests it is responsible for
+                - can access its successor
+                - if the ConcreteHandler can handle the request, it does so; 
+                otherwise it forwards the request to its successor
+
+            Client (ChainApp)
+                - initiates the request to a ConcreteHandler object on the chain 
     */
     /// </summary>
     static class Program
